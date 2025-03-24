05ec42bc0b86a7f5d51391b707e01c1c0f00ffe3
Mohammed1732001 <ososm493@gmail.com>
2025-03-24 08:09:06 +0200

    folder stracture backGrondService

---
+++ BackgroundService/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import initApp from "./src/app.router.js";
+dotenv.config();
+
+const port = process.env.PORT || 1732002;
+
+
+initApp(express, app)
+
+app.listen(port, () => {
+    console.log(`BackgroundService app listening on port ${port}`);    
+})
