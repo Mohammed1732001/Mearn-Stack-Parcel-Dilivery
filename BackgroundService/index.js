00cb27df34520a41060c42786d0dfb0f836b4961
Mohammed1732001 <ososm493@gmail.com>
2025-03-30 07:57:13 +0200

    done panding parcel and deliverd parcel

--- BackgroundService/index.js
+++ BackgroundService/index.js
@@ -3,6 +3,7 @@ const app = express();
 import dotenv from "dotenv";
 import initApp from "./src/app.router.js";
 import sendWelcomeEmail from "./utils/EmailService/welcomEmail.js";
+import sendParcelPandingEmail from "./utils/EmailService/pandingParcel.js";
 
 
 dotenv.config();
@@ -11,7 +12,6 @@ const port = process.env.PORT || 1732002;
 
 
 initApp(express, app)
-sendWelcomeEmail()
 
 app.listen(port, () => {
     console.log(`BackgroundService app listening on port ${port}`);
