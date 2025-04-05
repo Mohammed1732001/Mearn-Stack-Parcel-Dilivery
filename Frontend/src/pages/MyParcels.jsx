449d9c31f2a98b39546790aa17c65acb4e29a44b
Mohammed1732001 <ososm493@gmail.com>
2025-04-05 17:28:20 +0200

    starting working in front end

---
+++ Frontend/src/pages/MyParcels.jsx
@@ -0,0 +1,8 @@
+
+function MyParcels() {
+  return (
+    <div>MyParcels</div>
+  )
+}
+
+export default MyParcels
---
+++ Frontend/src/pages/Parcel.jsx
@@ -0,0 +1,8 @@
+
+function Parcel() {
+  return (
+    <div>Parcel</div>
+  )
+}
+
+export default Parcel
